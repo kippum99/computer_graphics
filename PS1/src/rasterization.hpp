212f9ce544c3c1787e9e06f4ebe0152a5dee9b3f
June Kim <jooeun@caltech.edu>
2020-10-16 16:18:52 +0100

    Finished PS1 but with bugs

--- PS1/src/rasterization.hpp
+++ PS1/src/rasterization.hpp
@@ -3,7 +3,10 @@
 
 #include "object.hpp"
 
+
 void rasterize_object(Object &obj, int xres, int yres, int **grid);
-void rasterize_line(int x1, int x2, int y1, int y2, int **grid);
+void rasterize_line(int x1, int y1, int x2, int y2, int xres, int yres,
+                    int **grid);
+
 
 #endif
